64ae313f8f1baabd08812d3dd9ee80462d04a4bb
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 01:23:45 +0200

    Update commands.h

--- src/server/commands.h
+++ src/server/commands.h
@@ -6,6 +6,13 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// Function to handle user registration
+const char *register_user(const char *username, const char *password);
 
 void *handle_connection(void *arg);
 
