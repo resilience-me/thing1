165b421ff2d761633b5f6f3d4fe18894100c01a7
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 00:29:35 +0200

    Create commands.c

---
+++ src/server/commands.c
@@ -0,0 +1,42 @@
+#include "commands.h"
+
+void *handle_connection(void *arg) {
+    SSL *ssl = (SSL *)arg;
+    const int read_size = 256;
+    char buffer[read_size];
+    int bytes;
+
+    // Perform SSL handshake
+    if (SSL_accept(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        goto cleanup;
+    }
+
+    // Receive and process commands from the client
+    while (1) {
+        bytes = SSL_read(ssl, buffer, sizeof(buffer));
+        if (bytes <= 0) {
+            break; // No more data or error occurred
+        }
+        buffer[bytes] = '\0';
+        printf("Received: %s\n", buffer);
+
+        // Parse the received command
+        char *token = strtok(buffer, "\n");
+        if (token != NULL) {
+            printf("Received token: %s\n", token);
+
+            if (strcmp(token, "LOGIN") == 0) {
+                SSL_write(ssl, "LOGIN_SUCCESS", strlen("LOGIN_SUCCESS"));
+            } else if (strcmp(token, "REGISTER") == 0) {
+                SSL_write(ssl, "REGISTER_SUCCESS", strlen("REGISTER_SUCCESS"));
+            } else {
+                SSL_write(ssl, "INVALID_COMMAND", strlen("INVALID_COMMAND"));
+            }
+        }
+    }
+
+cleanup:
+    SSL_free(ssl);
+    pthread_exit(NULL);
+}
