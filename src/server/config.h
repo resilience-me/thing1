370efb9dd2942ac9078863cd641e40b16010c9fb
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 00:31:02 +0200

    Create config.h

---
+++ src/server/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Network configuration
+#define PORT 2012
+#define CERTIFICATE_FILE "/etc/letsencrypt/live/jipple.net/fullchain.pem"
+#define PRIVATE_KEY_FILE "/etc/letsencrypt/live/jipple.net/privkey.pem"
+
+// Other configuration settings...
+
+#endif /* CONFIG_H */
