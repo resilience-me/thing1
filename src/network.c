663350828767bb07a33026e643ee276686690015
resilience-me <emailsareobsolete@gmail.com>
2024-05-01 23:15:55 +0200

    Create network.c

---
+++ src/network.c
@@ -0,0 +1,74 @@
+#include "network.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <openssl/err.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+void init_openssl() {
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanup_openssl() {
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context() {
+    const SSL_METHOD *method = TLS_client_method();
+    SSL_CTX *ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx) {
+    SSL_CTX_set_default_verify_paths(ctx);
+}
+
+int open_connection(const char *hostname, int port) {
+    struct addrinfo hints, *res, *p;
+    int sock;
+    char port_str[6];
+    snprintf(port_str, sizeof(port_str), "%d", port);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(hostname, port_str, &hints, &res) != 0) {
+        perror("getaddrinfo failed");
+        exit(EXIT_FAILURE);
+    }
+
+    for (p = res; p != NULL; p = p->ai_next) {
+        sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sock == -1) continue;
+        if (connect(sock, p->ai_addr, p->ai_addrlen) == 0) break;
+        close(sock);
+    }
+
+    freeaddrinfo(res);
+    if (p == NULL) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return sock;
+}
+
+SSL* perform_ssl_handshake(SSL_CTX *ctx, int sock) {
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sock);
+    if (SSL_connect(ssl) != 1) {
+        ERR_print_errors_fp(stderr);
+        SSL_free(ssl);
+        exit(EXIT_FAILURE);
+    }
+    return ssl;
+}
