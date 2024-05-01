faef39afc43dc167b502e39bce6270e51d8f76d7
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 01:46:31 +0200

    Create database.h

---
+++ src/server/database.h
@@ -0,0 +1,12 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "config.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+// Function to create directories if they don't exist
+void create_directories();
+
+#endif /* DATABASE_H */
