80d1eba3b4c6898c12215bd40d3745d200b05e96
resilience-me <emailsareobsolete@gmail.com>
2024-05-01 23:38:14 +0200

    Update network.c

--- src/client/network.c
+++ src/client/network.c
@@ -72,3 +72,27 @@ SSL* perform_ssl_handshake(SSL_CTX *ctx, int sock) {
     }
     return ssl;
 }
+
+char* parse_url(const char *url, int *port) {
+    regex_t regex;
+    regmatch_t pmatch[4];
+    const char *pattern = "^([^:/]+)(:([0-9]+))?/?$";
+    char *hostname = NULL;
+
+    if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
+        fprintf(stderr, "Could not compile regex\n");
+        return NULL;
+    }
+
+    if (regexec(&regex, url, 4, pmatch, 0) == 0) {
+        hostname = strndup(url + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+        if (pmatch[3].rm_so != -1) {  // Port is specified
+            *port = atoi(url + pmatch[3].rm_so);
+        }
+    } else {
+        fprintf(stderr, "URL format error\n");
+    }
+
+    regfree(&regex);
+    return hostname;
+}
