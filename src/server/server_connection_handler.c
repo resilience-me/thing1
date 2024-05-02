ca0105f5784b97bf96e3d97366ebac562790a244
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 09:21:40 +0200

    Update server_connection_handler.c

--- src/server/server_connection_handler.c
+++ src/server/server_connection_handler.c
@@ -2,25 +2,25 @@
 #include "server_connection_handler.h"
 #include "database.h" // Assuming you have functions to check account existence in your database module
 
+// Function to send a response to the client
+void send_response(SSL *ssl, const char *response) {
+    // Send the response to the client
+    SSL_write(ssl, response, strlen(response));
+}
+
 void handle_server_connection(SSL *ssl) {
     // Receive the command from the client
-    char command[MAX_COMMAND_LENGTH];
-    receive_command(ssl, command, sizeof(command));
+    char buffer[256];
+    SSL_read(ssl, buffer, max_length);
+    char *token = strtok(buffer, " ");
 
+    if(token == NULL) send_response(ssl, "INVALID_COMMAND");
     // Check if the received command is ACCOUNT_EXIST
-    if (strcmp(command, "ACCOUNT_EXIST") == 0) {
+    if (strcmp(token, "ACCOUNT_EXIST") == 0) {
         // Receive the username from the client
-        char username[MAX_USERNAME_LENGTH];
-        receive_username(ssl, username, sizeof(username));
-
-        // Check if the account exists in the database
-        if (account_exists(username)) {
-            // Send the response to the client indicating that the account exists
-            send_response(ssl, "ACCOUNT_EXISTS");
-        } else {
-            // Send the response to the client indicating that the account does not exist
-            send_response(ssl, "ACCOUNT_DOES_NOT_EXIST");
-        }
+        char *username = strtok(NULL, " ");
+        const char *account_exists_result = account_exists(username);
+        send_response(ssl, account_exists_result);
     } else {
         // Send an error response to the client indicating an invalid command
         send_response(ssl, "INVALID_COMMAND");
