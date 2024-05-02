6b256ade736f0fe097d60a0a3e2d5d512ad989ea
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 05:55:14 +0200

    Update main.c

--- src/server/main.c
+++ src/server/main.c
@@ -3,6 +3,32 @@
 #include "database.h"
 #include "config.h"
 
+void *handle_connection(void *arg) {
+    SSL *ssl = (SSL *)arg;
+    
+    // Perform SSL handshake
+    if (SSL_accept(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        goto cleanup;
+    }
+    
+    struct ProtocolHeader header;
+    SSL_read(ssl, &header, sizeof(header));
+
+    // Determine connection type based on the identifier
+    if (header.connectionType == CLIENT_CONNECTION) {
+        handle_client_connection(ssl);
+    } else if (header.connectionType == SERVER_CONNECTION) {
+        handle_server_connection(ssl);
+    } else {
+        // Invalid connection type
+    }
+
+cleanup:
+    SSL_free(ssl);
+    pthread_exit(NULL);
+}
+
 int main(int argc, char **argv) {
     // Create necessary directories if they do not exist
     initialize_database_directories();
