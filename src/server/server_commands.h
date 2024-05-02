10e051a2e33774295593e2ef63983101c83c8925
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 07:03:34 +0200

    Update server_commands.h

--- src/server/server_commands.h
+++ src/server/server_commands.h
@@ -4,12 +4,12 @@
 #include <openssl/ssl.h> // Include OpenSSL header
 
 // Function to send a query to the remote server using SSL
-void ssl_send_query(SSL *ssl, const char *query);
+void send_query(SSL *ssl, const char *query);
 
 // Function to receive a response from the remote server using SSL
-void ssl_receive_response(SSL *ssl, char *response, size_t max_length);
+void receive_response(SSL *ssl, char *response, size_t max_length);
 
 // Function to establish an SSL connection to a remote server
-SSL* ssl_establish_connection(const char *server_address, int port);
+SSL* establish_connection(const char *server_address, int port);
 
 #endif /* SERVER_COMMANDS_H */
