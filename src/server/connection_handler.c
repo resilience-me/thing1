509f460688ae6f2f18c77badad8ee914dfae5677
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 05:57:00 +0200

    Create connection_handler.c

---
+++ src/server/connection_handler.c
@@ -0,0 +1,25 @@
+void *handle_connection(void *arg) {
+    SSL *ssl = (SSL *)arg;
+    
+    // Perform SSL handshake
+    if (SSL_accept(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        goto cleanup;
+    }
+    
+    struct ProtocolHeader header;
+    SSL_read(ssl, &header, sizeof(header));
+
+    // Determine connection type based on the identifier
+    if (header.connectionType == CLIENT_CONNECTION) {
+        handle_client_connection(ssl);
+    } else if (header.connectionType == SERVER_CONNECTION) {
+        handle_server_connection(ssl);
+    } else {
+        // Invalid connection type
+    }
+
+cleanup:
+    SSL_free(ssl);
+    pthread_exit(NULL);
+}
