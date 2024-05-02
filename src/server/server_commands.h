a784fa68e2bbcd131556e76d09a2d717dcb93e49
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 09:29:39 +0200

    Update server_commands.h

--- src/server/server_commands.h
+++ src/server/server_commands.h
@@ -9,6 +9,8 @@ void send_query(SSL *ssl, const char *query);
 // Function to receive a response from the remote server using SSL
 void receive_response(SSL *ssl, char *response, size_t max_length);
 
+const char *account_exists(char *username);
+
 const char *send_account_exists_query(SSL *ssl, const char *username);
 
 // Function to establish an SSL connection to a remote server
