7a15e5fdb8e34d8cce2d38786e7e9724d7a42a9f
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 09:44:41 +0200

    Update server_connection_handler.c

--- src/server/server_connection_handler.c
+++ src/server/server_connection_handler.c
@@ -23,18 +23,16 @@ void handle_server_connection(SSL *ssl) {
     }
     char *token = strtok(buffer, " ");
 
-    if(token == NULL) send_response(ssl, buffer);
-    //if(token == NULL) send_response(ssl, "INVALID_COMMAND");
+    if(token == NULL) send_response(ssl, "INVALID_COMMAND");
 
     // Check if the received command is ACCOUNT_EXIST
-    if (strcmp(token, "ACCOUNT_EXIST") == 0) {
+    if (strcmp(token, "ACCOUNT_EXISTS") == 0) {
         // Receive the username from the client
         char *username = strtok(NULL, " ");
         const char *account_exists_result = account_exists(username);
         send_response(ssl, account_exists_result);
     } else {
         // Send an error response to the client indicating an invalid command
-        send_response(ssl, token);
-        //send_response(ssl, "INVALID_COMMAND");
+        send_response(ssl, "INVALID_COMMAND");
     }
 }
