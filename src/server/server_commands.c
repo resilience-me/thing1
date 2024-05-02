31b66d0a82881d2bf9f74f435ca1ec7814aeadfc
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 01:24:36 +0200

    Update server_commands.c

--- src/server/server_commands.c
+++ src/server/server_commands.c
@@ -74,69 +74,29 @@ const char *send_account_exists_query(SSL *ssl, const char *username) {
 }
 
 // Function to establish a connection to a remote server
-SSL* establish_connection(const char *server_address, char *portStr) {
-    int sockfd;
-    SSL_CTX *ctx;
-    SSL *ssl;
-
-    // Initialize SSL
+SSL* establish_connection(const char *server_address, const char *portStr) {
     SSL_library_init();
-    ctx = SSL_CTX_new(SSLv23_client_method());
+    SSL_CTX *ctx = SSL_CTX_new(SSLv23_client_method());
     if (ctx == NULL) {
         perror("SSL context creation error");
         return NULL;
     }
 
-    // Create socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation error");
+    struct addrinfo *res;
+    if (resolve_hostname(server_address, portStr, &res) != 0) {
         SSL_CTX_free(ctx);
         return NULL;
     }
 
-    // Resolve domain name to IP address
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // Use IPv4 or IPv6, whichever is available
-    hints.ai_socktype = SOCK_STREAM; // TCP socket
-    if (getaddrinfo(server_address, portStr, &hints, &res) != 0) {
-        perror("getaddrinfo");
-        close(sockfd);
-        SSL_CTX_free(ctx);
-        return NULL;
-    }
-
-    // Attempt to connect to each resolved IP address until successful
-    struct addrinfo *p;
-    for (p = res; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
-            perror("Socket creation error");
-            continue;
-        }
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) != -1) {
-            break; // Connection successful
-        }
-        close(sockfd); // Close socket if connection fails
-    }
-    if (p == NULL) {
-        perror("Connection failed");
-        freeaddrinfo(res);
-        close(sockfd);
-        SSL_CTX_free(ctx);
-        return NULL;
-    }
-
-    // Free addrinfo structure
+    int sockfd = create_and_connect_socket(res);
     freeaddrinfo(res);
 
-    // Set up SSL connection
-    ssl = SSL_new(ctx);
-    if (ssl == NULL) {
-        perror("SSL creation error");
-        close(sockfd);
+    if (sockfd == -1) {
         SSL_CTX_free(ctx);
         return NULL;
     }
+
+    SSL *ssl = SSL_new(ctx);
     SSL_set_fd(ssl, sockfd);
     if (SSL_connect(ssl) != 1) {
         perror("SSL connection error");
@@ -146,18 +106,6 @@ SSL* establish_connection(const char *server_address, char *portStr) {
         return NULL;
     }
 
-    // Free SSL context (it's no longer needed)
     SSL_CTX_free(ctx);
-    
-    // Construct and send the protocol header
-    struct ProtocolHeader header;
-    header.connectionType = SERVER_CONNECTION;
-    if (SSL_write(ssl, &header, sizeof(header)) != sizeof(header)) {
-        perror("Protocol header send failed");
-        close(sockfd);
-        SSL_free(ssl);
-        return NULL;
-    }
-
-    return ssl; // Return the SSL connection object
+    return ssl;
 }
