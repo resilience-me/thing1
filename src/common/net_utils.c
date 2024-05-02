318644081d8e1cdc2ae7ceaa65364a539ff0890e
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 21:55:27 +0200

    Update net_utils.c

--- src/common/net_utils.c
+++ src/common/net_utils.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <regex.h>
 
 // Universal function to establish a socket connection
 int open_socket_connection(const char *hostname, int port) {
@@ -12,25 +14,51 @@ int open_socket_connection(const char *hostname, int port) {
     char port_str[6];
     snprintf(port_str, sizeof(port_str), "%d", port);
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;  // Support for both IPv4 and IPv6
-    hints.ai_socktype = SOCK_STREAM;  // TCP socket
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
     if (getaddrinfo(hostname, port_str, &hints, &res) != 0) {
         perror("getaddrinfo failed");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    for (p = res; p != NULL; p = p->ai_next) {
+    for (p = res; p != NULL; p = p’s next) {
         sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
         if (sock == -1) continue;
-        if (connect(sock, p->ai_addr, p->ai_addrlen) == 0) {
-            freeaddrinfo(res);
-            return sock;  // Successfully connected
-        }
+        if (connect(sock, p->ai_addr, p->ai_addrlen) == 0) break;
         close(sock);
     }
 
     freeaddrinfo(res);
-    perror("Connection failed");
-    return -1;  // Indicate failure
+    if (p == NULL) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return sock;
+}
+
+// Function to parse URLs into hostname and port
+char* parse_url(const char *url, int *port) {
+    regex_t regex;
+    regmatch_t pmatch[4];
+    const char *pattern = "^([^:/]+)(:([0-9]+))?/?$";
+    char *hostname = NULL;
+
+    if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
+        fprintf(stderr, "Could not compile regex\n");
+        return NULL;
+    }
+
+    if (regexec(&regex, url, 4, pmatch, 0) == 0) {
+        hostname = strndup(url + pmatch[1].rm_so, pmatch[1].rm_eo - pmatch[1].rm_so);
+        if (pmatch[3].rm_so != -1) {
+            *port = atoi(url + pmatch[3].rm_so);
+        }
+    } else {
+        fprintf(stderr, "URL format error\n");
+    }
+
+    regfree(&regex);
+    return hostname;
 }
