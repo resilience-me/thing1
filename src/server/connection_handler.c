6960613745f9e1c5410e2d73b4f012f7cdab7d9f
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 00:28:15 +0200

    Update connection_handler.c

--- src/server/connection_handler.c
+++ src/server/connection_handler.c
@@ -17,33 +17,45 @@ void *handle_connection(void *arg) {
     ThreadArgs *thread_args = (ThreadArgs *)arg;
     SSL *ssl = NULL;
 
+    printf("Thread %lu: Handling connection...\n", pthread_self());  // Feedback message
+
     // Perform SSL handshake
     ssl = ssl_handshake(thread_args->ctx, thread_args->sock, SSL_accept);
     if (!ssl) {
-        fprintf(stderr, "SSL handshake failed\n");
+        fprintf(stderr, "Thread %lu: SSL handshake failed\n", pthread_self());  // Error message
         goto cleanup;
     }
-    
+
+    printf("Thread %lu: SSL handshake successful\n", pthread_self());  // Feedback message
+
     // Handle client or server connection based on the received protocol header
     struct ProtocolHeader header;
     if (SSL_read(ssl, &header, sizeof(header)) != sizeof(header)) {
-        fprintf(stderr, "Error reading protocol header\n");
+        fprintf(stderr, "Thread %lu: Error reading protocol header\n", pthread_self());  // Error message
         // Handle error appropriately
     } else {
+        printf("Thread %lu: Received protocol header\n", pthread_self());  // Feedback message
+
         if (header.connectionType == CLIENT_CONNECTION) {
+            printf("Thread %lu: Handling client connection\n", pthread_self());  // Feedback message
             handle_client_connection(ssl);
         } else if (header.connectionType == SERVER_CONNECTION) {
+            printf("Thread %lu: Handling server connection\n", pthread_self());  // Feedback message
             handle_server_connection(ssl);
         } else {
-            // Invalid connection type
+            fprintf(stderr, "Thread %lu: Invalid connection type\n", pthread_self());  // Error message
+            // Handle error appropriately
         }
     }
 
     // Clean up SSL resources
     SSL_free(ssl);
-    cleanup:
-        close(thread_args->sock);
-        SSL_CTX_free(thread_args->ctx);
-        free(thread_args);
-        pthread_exit(NULL);
+cleanup:
+    close(thread_args->sock);
+    SSL_CTX_free(thread_args->ctx);
+    free(thread_args);
+
+    printf("Thread %lu: Connection handling complete\n", pthread_self());  // Feedback message
+    pthread_exit(NULL);
 }
+
