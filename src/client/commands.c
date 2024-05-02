9f8aa622a3aa57bebe7ff7e7af3a9fb8f391454f
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 03:10:13 +0200

    Update commands.c

--- src/client/commands.c
+++ src/client/commands.c
@@ -39,15 +39,42 @@ void handle_login_and_register(char *loginOrRegister, SSL *ssl) {
     printf("Server response: %s\n", response);
 }
 
+void handle_login(SSL *ssl) {
+    handle_login_and_register("LOGIN", ssl);
+}
+void handle_register(SSL *ssl) {
+    handle_login_and_register("REGISTER", ssl);
+}
+
+void handle_logout(SSL *ssl) {
+    const char *logout_cmd = "LOGOUT";
+    // Sending the LOGOUT command to the server
+    SSL_write(ssl, logout_cmd, strlen(logout_cmd));
+
+    // Receiving response from the server
+    char response[256];
+    int bytes = SSL_read(ssl, response, sizeof(response) - 1);
+    if (bytes > 0) {
+        response[bytes] = '\0';  // Null-terminate the response
+        printf("Server response: %s\n", response);
+    } else if (bytes == 0) {
+        printf("Connection closed by server.\n");
+    } else {
+        printf("SSL read error.\n");
+    }
+}
+
 void interact_with_server(SSL *ssl) {
     char cmd[256];
     printf("Enter command (LOGIN, REGISTER, EXIT): ");
     while (fgets(cmd, sizeof(cmd), stdin) && strcmp(cmd, "EXIT\n") != 0) {
         cmd[strcspn(cmd, "\n")] = '\0';
         if (strcmp(cmd, "LOGIN") == 0) {
-            handle_login_and_register("LOGIN", ssl);
+            handle_login(ssl);
         } else if (strcmp(cmd, "REGISTER") == 0) {
-            handle_login_and_register("REGISTER", ssl);
+            handle_register(ssl);
+        } else if (strcmp(cmd, "LOGOUT") == 0) {
+            handle_logout(ssl);
         } else {
             printf("Unknown command. Please try again.\n");
         }
