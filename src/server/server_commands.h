1a8a60f8cea2063a87905510afd044804feb387d
resilience-me <emailsareobsolete@gmail.com>
2024-05-02 06:07:30 +0200

    Create server_commands.h

---
+++ src/server/server_commands.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_COMMANDS_H
+#define SERVER_COMMANDS_H
+
+// Function to send a query to the remote server
+void send_query(int sockfd, const char *query);
+
+// Function to receive a response from the remote server
+void receive_response(int sockfd, char *response, size_t max_length);
+
+// Function to establish a connection to a remote server
+int establish_connection(const char *server_address, int port);
+
+#endif /* SERVER_COMMANDS_H */
