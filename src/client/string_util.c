54ead3fdbb374c43e3fc4abb277c36c1b097f96a
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 05:43:20 +0200

    Create string_util.c

---
+++ src/client/string_util.c
@@ -0,0 +1,12 @@
+#include "string_util.h"
+#include <ctype.h> // for isalnum
+
+// Function to check if a username contains only valid characters
+int isValidUsername(const char *username) {
+    for (int i = 0; username[i] != '\0'; ++i) {
+        if (!(isalnum(username[i]) || username[i] == '_')) {
+            return 0; // Invalid username
+        }
+    }
+    return 1; // Valid username
+}
