fbdd2935f6507778f8f1ca4f94f4a6ed5c874941
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 19:53:03 +0200

    Update parse_string_util.c

--- src/client/parse_string_util.c
+++ src/client/parse_string_util.c
@@ -5,16 +5,6 @@
 #include <regex.h>     // For regex functions
 #include <string.h>    // For string manipulation functions
 
-// Function to check if a username contains only valid characters
-int isValidUsername(const char *username) {
-    for (int i = 0; username[i] != '\0'; ++i) {
-        if (!(isalnum(username[i]) || username[i] == '_')) {
-            return 0; // Invalid username
-        }
-    }
-    return 1; // Valid username
-}
-
 char* parse_url(const char *url, int *port) {
     regex_t regex;
     regmatch_t pmatch[4];
