97ac45dddb87a54fa726bc4c5422a653e9a2dc84
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 05:05:03 +0200

    Update network.h

--- src/client/network.h
+++ src/client/network.h
@@ -3,11 +3,7 @@
 
 #include <openssl/ssl.h>
 
-void init_openssl();
-void cleanup_openssl();
-SSL_CTX *create_context();
 void configure_context(SSL_CTX *ctx);
-SSL* perform_ssl_handshake(SSL_CTX *ctx, int sock);
 char* parse_url(const char *url, int *port);
 
 #endif
