eb36f496acf07969208dbb6fc3ec004376baeffb
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 21:33:14 +0200

    Update parse_string_util.h

--- src/client/parse_string_util.h
+++ src/client/parse_string_util.h
@@ -2,6 +2,6 @@
 #define PARSE_STRING_UTIL_H
 
 // Other string-related utility functions can be declared here
-char* parse_url(const char *url, int *port);
+char* parse_address_and_port(const char *url, int *port);
 
 #endif // PARSE_STRING_UTIL_H
