4b67c81585da794079453ad342d0fdbf50c33111
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 20:22:15 +0200

    Update ssl_utils.h

--- src/common/ssl_utils.h
+++ src/common/ssl_utils.h
@@ -13,4 +13,6 @@ SSL_CTX* create_ssl_client_context(void);
 // Function to perform SSL client handshake
 SSL* ssl_client_handshake(SSL_CTX *ctx, int sock);
 
+SSL* setup_ssl_connection(SSL_CTX *ctx, int sockfd);
+
 #endif // SSL_UTILS_H
