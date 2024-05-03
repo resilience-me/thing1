24cf6b0fe873b5e1bce3806f15688728a6da8b2e
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 03:17:05 +0200

    Update net_utils.c

--- src/common/net_utils.c
+++ src/common/net_utils.c
@@ -53,20 +53,3 @@ int open_connection(const char *hostname, int port) {
 
     return sock;
 }
-
-SSL* ssl_client_handshake(SSL_CTX *ctx, int sock) {
-    SSL *ssl = SSL_new(ctx);
-    if (!ssl) {
-        ERR_print_errors_fp(stderr);
-        return NULL;  // Ensure SSL struct creation didn't fail
-    }
-
-    SSL_set_fd(ssl, sock);
-    if (SSL_connect(ssl) != 1) {
-        ERR_print_errors_fp(stderr);
-        SSL_free(ssl);
-        return NULL;  // Return NULL to indicate handshake failure
-    }
-
-    return ssl;  // Return the SSL object on success
-}
