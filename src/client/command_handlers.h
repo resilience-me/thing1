0a855e42decdb2eb32e6e5a4e526d706d9d656ac
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 05:39:19 +0200

    Create command_handlers.h

---
+++ src/client/command_handlers.h
@@ -0,0 +1,13 @@
+#ifndef COMMAND_HANDLERS_H
+#define COMMAND_HANDLERS_H
+
+#include <openssl/ssl.h>
+
+// Function declarations
+void handle_login(SSL *ssl);
+void handle_register(SSL *ssl);
+void handle_logout(SSL *ssl);
+void handle_delete_account(SSL *ssl);
+void handle_add_connection(SSL *ssl);
+
+#endif // COMMAND_HANDLERS_H
