3b0965a5e1dfd3ff0d32beb267a01ca3d9cdbac6
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 20:08:07 +0200

    Update network.h

--- src/server/network.h
+++ src/server/network.h
@@ -15,4 +15,7 @@ SSL_CTX *create_server_context();
 void configure_server_context(SSL_CTX *ctx);
 int create_socket(int port);
 
+// Function to establish an SSL connection to a remote server
+SSL* establish_connection(const char *server_address, int port);
+
 #endif /* NETWORK_H */
