11a33612eb7e345b9e213776ce1401e6b99d9356
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 05:44:15 +0200

    Update commands.h

--- src/client/commands.h
+++ src/client/commands.h
@@ -3,11 +3,6 @@
 
 #include <openssl/ssl.h>
 
-void handle_login(SSL *ssl);
-void handle_register(SSL *ssl);
-void handle_logout(SSL *ssl);
-void handle_delete_account(SSL *ssl);
-void handle_add_connection(SSL *ssl);
 void interact_with_server(SSL *ssl);
 
 #endif
