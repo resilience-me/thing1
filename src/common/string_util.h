a7a628eec06fc02be1d20b2a3d07b2b07f35bdad
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 19:52:48 +0200

    Create string_util.h

---
+++ src/common/string_util.h
@@ -0,0 +1,7 @@
+#ifndef STRING_UTIL_H
+#define STRING_UTIL_H
+
+// Checks if the given username contains only valid characters
+int isValidUsername(const char *username);
+
+#endif // PARSE_STRING_UTIL_H
