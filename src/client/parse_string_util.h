aefad9aed76589cf35bc5d660c36fd4a5a12012b
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 19:53:10 +0200

    Update parse_string_util.h

--- src/client/parse_string_util.h
+++ src/client/parse_string_util.h
@@ -1,9 +1,6 @@
 #ifndef PARSE_STRING_UTIL_H
 #define PARSE_STRING_UTIL_H
 
-// Checks if the given username contains only valid characters
-int isValidUsername(const char *username);
-
 // Other string-related utility functions can be declared here
 char* parse_url(const char *url, int *port);
 
