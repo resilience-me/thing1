867b1b034551a94b82ea699cd39afac195da5878
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 20:39:35 +0200

    Update network.c

--- src/server/network.c
+++ src/server/network.c
@@ -69,10 +69,26 @@ int create_socket(int port) {
 
 // Function to establish a connection to a remote server
 SSL* establish_connection(const char *server_address, int port) {
+    init_openssl();
 
-    SSL *ssl = establish_ssl_client_connection(server_address, port);
+    SSL_CTX *ctx = create_ssl_client_context();
+    if (ctx == NULL) {
+        fprintf(stderr, "Failed to create SSL context\n");
+        return NULL;
+    }
+
+    configure_client_context(ctx);
+
+    int sockfd = open_connection(server_address, port);
+    if (sockfd == -1) {
+        SSL_CTX_free(ctx);
+        return NULL;
+    }
+
+    SSL *ssl = ssl_client_handshake(ctx, sockfd);
     if (ssl == NULL) {
-        fprintf(stderr, "Failed to establish SSL connection\n");
+        close(sockfd);
+        SSL_CTX_free(ctx);
         return NULL;
     }
 
