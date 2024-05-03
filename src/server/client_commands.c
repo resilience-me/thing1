5c43828e364557a8144af53cafe28f8f4c24d563
resilience-me <emailsareobsolete@gmail.com>
2024-05-03 19:53:56 +0200

    Update client_commands.c

--- src/server/client_commands.c
+++ src/server/client_commands.c
@@ -3,17 +3,9 @@
 #include "server_commands.h"
 #include "database.h"
 #include "server_config.h"
-#include <ctype.h>
+#include "string_util.h"
 
-// Function to check if a username contains only valid characters
-int isValidUsername(const char *username) {
-    for (int i = 0; username[i] != '\0'; ++i) {
-        if (!(isalnum(username[i]) || username[i] == '_')) {
-            return 0; // Invalid username
-        }
-    }
-    return 1; // Valid username
-}
+#include <ctype.h>
 
 // Function to handle user registration
 const char *register_user(const char *username, const char *password) {
