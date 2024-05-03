6a707330ebafd7d19034e749400a40b511987d6e
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 00:39:38 +0200

    Create client_command_processor.h

---
+++ src/server/client_command_processor.h
@@ -0,0 +1,9 @@
+#ifndef CLIENT_COMMAND_PROCESSOR_H
+#define CLIENT_COMMAND_PROCESSOR_H
+
+#include "client_connection_handler.h"
+#include <openssl/ssl.h>  // Needed for SSL type in function prototypes
+
+void dispatch_command(SSL *ssl, Session *session, const char *command, const char **args);
+
+#endif // CLIENT_COMMAND_PROCESSOR_H
