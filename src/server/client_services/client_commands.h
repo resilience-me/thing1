c4f70d7746346287a03cc9d9a60dc99e072c92a6
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 08:31:01 +0200

    Update client_commands.h

--- src/server/client_services/client_commands.h
+++ src/server/client_services/client_commands.h
@@ -1,16 +1,13 @@
 #ifndef CLIENT_COMMANDS_H
 #define CLIENT_COMMANDS_H
 
-#include <openssl/ssl.h>  // Needed for SSL type in function prototypes
+#include "command_defs.h"
 #include "session_defs.h"
 
 typedef const char *(*CommandHandler)(Session *session, const char *args);
 
-typedef struct {
-    char *name;
-    CommandHandler handler;
-} Command;
+extern Command *commands;  // Making the commands array available externally
 
-extern Command commands[];  // Making the commands array available externally
+extern void *client_command_handlers[];
 
 #endif // CLIENT_COMMANDS_H
