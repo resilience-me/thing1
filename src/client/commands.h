f746fa34e880b9df0e336265cda503f37077f02f
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 06:47:13 +0200

    Update commands.h

--- src/client/commands.h
+++ src/client/commands.h
@@ -1,14 +1,10 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include "command_defs.h"
 #include <openssl/ssl.h>
 
 typedef void (*CommandHandler)(SSL*);
-
-typedef struct {
-    const char* name;
-    CommandHandler handler;
-} Command;
 
 extern Command commands[];  // Makes the commands array available to other files
 
