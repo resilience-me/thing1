b66f12272765a7959ad6cb9914e129c912fc74b8
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 06:34:56 +0200

    Update server_commands.c

--- src/server/server_services/server_commands.c
+++ src/server/server_services/server_commands.c
@@ -1,1 +1,7 @@
+// #include "server_commands.h"  // Include the header file for declarations
+// #include "server_command_handlers.h"
 
+// Command commands[] = {
+//     {"ACCOUNT_EXISTS", send_account_exists_query},
+//     {NULL, NULL, 0}  // Terminating entry
+// };
