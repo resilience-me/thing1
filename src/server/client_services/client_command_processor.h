a5169a8b46845b47166f419f8ba78950dbed1ff1
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 02:11:21 +0200

    Update client_command_processor.h

--- src/server/client_services/client_command_processor.h
+++ src/server/client_services/client_command_processor.h
@@ -4,6 +4,6 @@
 #include "session_defs.h"
 #include <openssl/ssl.h>  // Needed for SSL type in function prototypes
 
-void dispatch_command(SSL *ssl, Session *session, const char *command, const char **args);
+void dispatch_command(SSL *ssl, Session *session, const char *command, const char *args);
 
 #endif // CLIENT_COMMAND_PROCESSOR_H
