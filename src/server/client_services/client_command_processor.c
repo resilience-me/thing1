52cea8ead6c0821d3a305847dee3462394c99809
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 02:10:06 +0200

    Update client_command_processor.c

--- src/server/client_services/client_command_processor.c
+++ src/server/client_services/client_command_processor.c
@@ -2,7 +2,7 @@
 #include <string.h>    // For strcmp and strlen
 #include <openssl/ssl.h>  // For SSL_write
 
-void dispatch_command(SSL *ssl, Session *session, const char *command, const char **args) {
+void dispatch_command(SSL *ssl, Session *session, const char *command, const char *args) {
     for (int i = 0; commands[i].name != NULL; i++) {
         if (strcmp(command, commands[i].name) == 0) {
             const char *result;
