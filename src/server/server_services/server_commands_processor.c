3374c37b57d26988380c0ceb6262794eeaea9a75
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 06:32:30 +0200

    Create server_commands_processor.c

---
+++ src/server/server_services/server_commands_processor.c
@@ -0,0 +1,11 @@
+// #include "server_commands.h"  // Include the header file for declarations
+// #include <string.h>    // For strcmp and strlen
+// #include <openssl/ssl.h>  // For SSL_write
+
+// const char* dispatch_command(SSL *ssl, const char *command, const char *args) {
+//     for (int i = 0; server_commands[i].name != NULL; i++) {
+//         if (strcmp(command, server_commands[i].name) == 0) {
+//             return server_commands[i].handler(session, args); // Execute the handler
+//         }
+//     }
+// }
