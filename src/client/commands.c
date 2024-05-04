3bd2819b52a28e91130ae4a305588c3be34d5589
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 08:18:16 +0200

    Update commands.c

--- src/client/commands.c
+++ src/client/commands.c
@@ -17,21 +17,3 @@ void *command_handlers[] = {
 
 // Constructing the commands array
 Command *commands = NULL;
-
-void dispatch_command(SSL *ssl, const char *cmd) {
-    int found = 0;
-
-    for (Command *command = commands; command->name != NULL; command++) {
-        if (strcmp(cmd, command->name) == 0) {
-            if (command->handler) {  // Check if handler is not NULL
-                ((CommandHandler)command->handler)(ssl);
-            }
-            found = 1;
-            break;
-        }
-    }
-
-    if (!found) {
-        printf("Unknown command. Please try again.\n");
-    }
-}
