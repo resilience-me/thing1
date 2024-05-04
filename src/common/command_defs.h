a932a273f510a6c7c6760b2ee27ffebe6cd5a5df
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 06:50:41 +0200

    Update command_defs.h

--- src/common/command_defs.h
+++ src/common/command_defs.h
@@ -1,10 +1,10 @@
-#ifndef COMMAND_DEFS_H
-#define COMMAND_DEFS_H
+// #ifndef COMMAND_DEFS_H
+// #define COMMAND_DEFS_H
 
-// Generic command structure applicable to both client and server
-typedef struct {
-    char *name;
-    void *handler;  // Generic pointer to any function, allows for flexible handling
-} Command;
+// // Generic command structure applicable to both client and server
+// typedef struct {
+//     char *name;
+//     void *handler;  // Generic pointer to any function, allows for flexible handling
+// } Command;
 
-#endif // COMMAND_DEFS_H
+// #endif // COMMAND_DEFS_H
