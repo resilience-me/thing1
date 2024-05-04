2615cba562d3907f3ecdaaf3b81ff76d109303a5
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 08:38:43 +0200

    Update client_commands.c

--- src/server/client_services/client_commands.c
+++ src/server/client_services/client_commands.c
@@ -1,15 +1,6 @@
 #include "client_commands.h"  // Include the header file for declarations
 #include "client_command_handlers.h"
 
-// Command commands[] = {
-//     {"LOGIN", handle_login_user},
-//     {"LOGOUT", handle_logout_user},
-//     {"REGISTER", handle_register_user},
-//     {"DELETE_ACCOUNT", handle_delete_user},
-//     {"ADD_CONNECTION", handle_add_connection},
-//     {NULL, NULL}  // Terminating entry
-// };
-
 // Array of function pointers to command handlers
 void *client_command_handlers[] = {
     (void*)handle_login_user,
