2b0fd93784c87e5c92d98fd5dbb5b2f71beb55ef
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 08:15:25 +0200

    Create connection_handler.c

---
+++ src/client/connection_handler.c
@@ -0,0 +1,36 @@
+#include "connection_handler.h"
+#include "command_handlers.h"
+#include "commands.h"
+#include "command_defs.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void interact_with_server(SSL *ssl) {
+    // Initialize command array
+    commands = initialize_commands(client_command_names, command_handlers);
+    
+    char cmd[256];
+    char command_list[1024] = "Enter command (";  // Starting part of the command prompt
+
+    // Generate command list string from the commands array
+    for (Command *command = commands; command->name != NULL; command++) {
+        strcat(command_list, command->name);
+        if ((command + 1)->name != NULL) {
+            strcat(command_list, ", ");  // Add comma only if not the last command
+        }
+    }
+    strcat(command_list, "): ");  // Close the prompt string
+
+    printf("%s", command_list);  // Use the dynamically created list in the prompt
+
+    while (fgets(cmd, sizeof(cmd), stdin) && strcmp(cmd, "EXIT\n") != 0) {
+        cmd[strcspn(cmd, "\n")] = '\0';  // Strip newline
+        
+        dispatch_command(ssl, cmd);  // Delegate command handling to the dispatch function
+
+        printf("%s", command_list);  // Use the dynamically created list in the prompt
+    }
+    free(commands);
+}
