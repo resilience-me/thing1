6f08f9f83df7736d6183520eed5e302c4675eb25
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 08:17:54 +0200

    Update command_processor.c

--- src/client/command_processor.c
+++ src/client/command_processor.c
@@ -1,36 +1,24 @@
-#include "command_processor.h"
-#include "command_handlers.h"
-#include "commands.h"
-#include "command_defs.h"
-
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <openssl/ssl.h>
+#include "commands.h"
+#include "command_processor.h"
+#include "command_defs.h"
 
-void interact_with_server(SSL *ssl) {
-    // Initialize command array
-    commands = initialize_commands(client_command_names, command_handlers);
-    
-    char cmd[256];
-    char command_list[1024] = "Enter command (";  // Starting part of the command prompt
+void dispatch_command(SSL *ssl, const char *cmd) {
+    int found = 0;
 
-    // Generate command list string from the commands array
     for (Command *command = commands; command->name != NULL; command++) {
-        strcat(command_list, command->name);
-        if ((command + 1)->name != NULL) {
-            strcat(command_list, ", ");  // Add comma only if not the last command
+        if (strcmp(cmd, command->name) == 0) {
+            if (command->handler) {  // Check if handler is not NULL
+                ((CommandHandler)command->handler)(ssl);
+            }
+            found = 1;
+            break;
         }
     }
-    strcat(command_list, "): ");  // Close the prompt string
 
-    printf("%s", command_list);  // Use the dynamically created list in the prompt
-
-    while (fgets(cmd, sizeof(cmd), stdin) && strcmp(cmd, "EXIT\n") != 0) {
-        cmd[strcspn(cmd, "\n")] = '\0';  // Strip newline
-        
-        dispatch_command(ssl, cmd);  // Delegate command handling to the dispatch function
-
-        printf("%s", command_list);  // Use the dynamically created list in the prompt
+    if (!found) {
+        printf("Unknown command. Please try again.\n");
     }
-    free(commands);
 }
