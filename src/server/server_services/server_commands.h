563b9a9b36f6de8bc596e5df7d49eb026899bd9d
resilience-me <emailsareobsolete@gmail.com>
2024-05-04 08:46:15 +0200

    Update server_commands.h

--- src/server/server_services/server_commands.h
+++ src/server/server_services/server_commands.h
@@ -1,15 +1,14 @@
 #ifndef CLIENT_COMMANDS_H
 #define CLIENT_COMMANDS_H
 
-#include <openssl/ssl.h>  // Needed for SSL type in function prototypes
+#define SERVER_COMMAND_NAMES \
+    "ACCOUNT_EXISTS", \
+    NULL  // Ensure the list is properly terminated with NULL
+
+#include "command_defs.h"
 #include "session_defs.h"
 
-typedef const char *(*CommandHandler)(Session *session, const char *args);
-
-typedef struct {
-    char *name;
-    CommandHandler handler;
-} Command;
+typedef const char *(*ServerCommandHandler)(Session *session, const char *args);
 
 extern Command server_commands[];  // Making the commands array available externally
 
