b5ad90cb69eb5571bbca17b8e2263cf0ff663b6a
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 06:49:02 +0200

    Update commands.c

--- src/server/commands.c
+++ src/server/commands.c
@@ -1,3 +1,8 @@
+#include "client_commands.h"
+#include "server_commands.h"
+#include "server_as_client_commands.h"
+#include "server_as_server_commands.h"
+
 void initialize_all_commands() {
     client_commands = initialize_commands(client_command_names, client_command_handlers);
     server_as_client_commands = initialize_commands(server_command_names, server_as_client_command_handlers);
