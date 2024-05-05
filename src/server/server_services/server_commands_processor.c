8a02caba5e6c6b465620cc429741a20ebb3df970
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 04:14:36 +0200

    Update server_commands_processor.c

--- src/server/server_services/server_commands_processor.c
+++ src/server/server_services/server_commands_processor.c
@@ -1,12 +1,12 @@
-// #include "server_commands.h"  // Include the header file for declarations
-// #include "command_defs.h"  // Include the generic command handling definitions
-// #include <string.h>    // For strcmp and strlen
-// #include <openssl/ssl.h>  // For SSL_write
+#include "server_commands.h"  // Include the header file for declarations
+#include "command_defs.h"  // Include the generic command handling definitions
+#include <string.h>    // For strcmp and strlen
+#include <openssl/ssl.h>  // For SSL_write
 
-// const char* dispatch_command(SSL *ssl, const char *command, const char *args) {
-//     void *handler = find_command_handler(command, server_as_client_commands);
-//     if (handler) {
-//         return ((ServerAsClientCommandHandler)handler)(ssl, args);  // Execute the handler, assuming it exists
-//     }
-//     return NULL;  // Return NULL or some appropriate error handling code
-// }
+const char* dispatch_command(SSL *ssl, const char *command, const char *args) {
+    void *handler = find_command_handler(command, server_as_client_commands);
+    if (handler) {
+        return ((ServerAsClientCommandHandler)handler)(ssl, args);  // Execute the handler, assuming it exists
+    }
+    return NULL;  // Return NULL or some appropriate error handling code
+}
