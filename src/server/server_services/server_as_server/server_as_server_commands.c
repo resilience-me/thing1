d66007a498ed582a120592f926194fa139641a54
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 06:08:49 +0200

    Create server_as_server_commands.c

---
+++ src/server/server_services/server_as_server/server_as_server_commands.c
@@ -0,0 +1,9 @@
+#include "command_defs.h"
+// #include "server_as_server_command_handlers.h"
+#include <stddef.h>
+
+// Array of function pointers to command handlers
+void *server_as_server_command_handlers[] = {
+    NULL  // Include NULL for completeness
+};
+Command *server_as_server_commands = NULL;
