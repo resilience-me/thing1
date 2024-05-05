f78c3ae49d440c54a65c64d71da2e2be7d2bba61
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 08:25:47 +0200

    Update command_handlers.h

--- src/client/command_handlers.h
+++ src/client/command_handlers.h
@@ -9,5 +9,6 @@ void handle_register(SSL *ssl);
 void handle_logout(SSL *ssl);
 void handle_delete_account(SSL *ssl);
 void handle_add_connection(SSL *ssl);
+void handle_set_trustline(SSL *ssl);
 
 #endif // COMMAND_HANDLERS_H
