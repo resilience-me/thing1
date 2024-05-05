c88afce5465d0abe9f155dec09508a50073dbfb0
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 05:22:47 +0200

    Create server_as_client_commands.c

---
+++ src/server/server_services/server_as_client/server_as_client_commands.c
@@ -0,0 +1,16 @@
+#include "command_defs.h"
+#include <stddef.h>
+// #include "server_commands.h"  // Include the header file for declarations
+// #include "server_command_handlers.h"
+
+// Command server_commands[] = {
+//     {"ACCOUNT_EXISTS", send_account_exists_query},
+//     {NULL, NULL, 0}  // Terminating entry
+// };
+
+// // Array of function pointers to command handlers
+// void *server_as_client_command_handlers[] = {
+//     (void*)send_account_exists_query,
+//     NULL  // Include NULL for completeness
+// };
+Command *server_as_client_commands = NULL;
