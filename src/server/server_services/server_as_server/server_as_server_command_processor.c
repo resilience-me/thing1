751ad276179c71728c4925b080b5b330cd7049eb
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 02:20:51 +0200

    Update server_as_server_command_processor.c

--- src/server/server_services/server_as_server/server_as_server_command_processor.c
+++ src/server/server_services/server_as_server/server_as_server_command_processor.c
@@ -1,14 +1,14 @@
 // #include "server_commands.h"  // Include the header file for declarations
+// #include "command_defs.h"  // Include the generic command handling definitions
 // #include <string.h>    // For strcmp and strlen
 // #include <openssl/ssl.h>  // For SSL_write
 
 // void dispatch_command(SSL *ssl, const char *command, const char *args) {
-//     for (int i = 0; server_as_server_commands[i].name != NULL; i++) {
-//         if (strcmp(command, server_as_server_commands[i].name) == 0) {
-//             const char *result = ((ServerAsServerCommandHandler)server_as_server_commands[i].handler)(session, args);
-//             SSL_write(ssl, result, strlen(result)); // Send result back to client
-//             return;
-//         }
+//     void *handler = find_command_handler(command, server_as_server_commands);
+//     if (handler) {
+//         const char *result = ((ServerAsServerCommandHandler)handler)(session, args);
+//         SSL_write(ssl, result, strlen(result));  // Send result back to the other server
+//     } else {
+//         SSL_write(ssl, "INVALID_COMMAND", strlen("INVALID_COMMAND"));
 //     }
-//     SSL_write(ssl, "INVALID_COMMAND", strlen("INVALID_COMMAND"));
 // }
