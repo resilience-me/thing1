42f07df02eee3a9a37954e0fcecec6171840b5bc
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 06:48:16 +0200

    Create commands.c

---
+++ src/server/commands.c
@@ -0,0 +1,11 @@
+void initialize_all_commands() {
+    client_commands = initialize_commands(client_command_names, client_command_handlers);
+    server_as_client_commands = initialize_commands(server_command_names, server_as_client_command_handlers);
+    server_as_server_commands = initialize_commands(server_command_names, server_as_client_command_handlers);
+}
+
+void free_all_commands() {
+    free(client_commands);
+    free(server_as_client_commands);
+    free(server_as_server_commands);
+}
