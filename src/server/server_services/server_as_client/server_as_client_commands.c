4a065241adcac536cf203ed83520683b650d5f07
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 05:24:53 +0200

    Update server_as_client_commands.c

--- src/server/server_services/server_as_client/server_as_client_commands.c
+++ src/server/server_services/server_as_client/server_as_client_commands.c
@@ -8,9 +8,9 @@
 //     {NULL, NULL, 0}  // Terminating entry
 // };
 
-// // Array of function pointers to command handlers
-// void *server_as_client_command_handlers[] = {
-//     (void*)send_account_exists_query,
-//     NULL  // Include NULL for completeness
-// };
+// Array of function pointers to command handlers
+void *server_as_client_command_handlers[] = {
+    (void*)send_account_exists_query,
+    NULL  // Include NULL for completeness
+};
 Command *server_as_client_commands = NULL;
