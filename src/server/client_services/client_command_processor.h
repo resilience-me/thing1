d7145c9505cc84c13875f58bdb9abe813f1f1194
resilience-me <emailsareobsolete@gmail.com>
2024-05-05 08:05:40 +0200

    Delete src/server/client_services/client_command_processor.h

--- src/server/client_services/client_command_processor.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIENT_COMMAND_PROCESSOR_H
-#define CLIENT_COMMAND_PROCESSOR_H
-
-#include "session_defs.h"
-#include <openssl/ssl.h>  // Needed for SSL type in function prototypes
-
-void dispatch_command(SSL *ssl, Session *session, const char *command, const char *args);
-
-#endif // CLIENT_COMMAND_PROCESSOR_H
