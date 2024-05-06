781804013786cca5b8ca48e1ee6d0ae3252a90a5
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 05:59:36 +0200

    Update server_as_server_command_handlers.h

--- src/server/server_services/server_as_server/server_as_server_command_handlers.h
+++ src/server/server_services/server_as_server/server_as_server_command_handlers.h
@@ -1,1 +1,12 @@
+#ifndef SERVER_AS_SERVER_COMMAND_HANDLERS_H
+#define SERVER_AS_SERVER_COMMAND_HANDLERS_H
 
+/**
+ * Handle the SET_TRUSTLINE command received from the client.
+ * 
+ * @param args The arguments received with the command.
+ * @return A response string indicating the result of the command execution.
+ */
+const char *server_as_server_handle_set_trustline(const char *args);
+
+#endif /* SERVER_AS_SERVER_COMMAND_HANDLERS_H */
