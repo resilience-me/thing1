4ca2343bb9f9428934866ef18cfd6bc311a0200a
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 06:09:45 +0200

    Update server_connection_handler.c

--- src/server/server_services/server_as_server/server_connection_handler.c
+++ src/server/server_services/server_as_server/server_connection_handler.c
@@ -15,19 +15,16 @@ void handle_server_connection(SSL *ssl) {
     char buffer[read_size];
     int bytes = SSL_read(ssl, buffer, sizeof(buffer));
     buffer[bytes] = '\0';  // Ensure null termination
-    
-    char *token = strtok(buffer, " ");
 
-    if(token == NULL) send_response(ssl, "INVALID_COMMAND");
+    // Find the end of the first word
+    char *end_of_command = strchr(buffer, ' ');
+    char *command = buffer;
+    char *arguments = NULL;
 
-    // Check if the received command is ACCOUNT_EXIST
-    if (strcmp(token, "ACCOUNT_EXISTS") == 0) {
-        // Receive the username from the client
-        char *username = strtok(NULL, " ");
-        const char *account_exists_result = account_exists(username);
-        send_response(ssl, account_exists_result);
-    } else {
-        // Send an error response to the client indicating an invalid command
-        send_response(ssl, "INVALID_COMMAND");
+    if (end_of_command) {
+        *end_of_command = '\0';  // Null-terminate the command
+        arguments = end_of_command + 1;  // Arguments start after the space
     }
+    // Dispatch command with the whole arguments string
+    server_as_server_dispatch_command(ssl, command, arguments);
 }
