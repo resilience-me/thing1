f5d67ee90e2e1f534aa7b4546d59c062378bc569
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 09:30:10 +0200

    Update connection_handler.c

--- src/server/connection_handler.c
+++ src/server/connection_handler.c
@@ -14,26 +14,39 @@
 
 void *handle_connection(void *arg) {
     SSL *ssl = (SSL *)arg;
-    
-    // Perform SSL handshake
-    if (SSL_accept(ssl) <= 0) {
-        ERR_print_errors_fp(stderr);
+
+    struct ProtocolHeader header;
+
+    // Read the connection type message from the client
+
+    if (recv(SSL_get_fd(ssl), &header, sizeof(header), 0) <= 0) {
+        perror("Error receiving connection type");
         goto cleanup;
     }
-    
-    struct ProtocolHeader header;
-    SSL_read(ssl, &header, sizeof(header));
 
     // Determine connection type based on the identifier
     if (header.connectionType == CLIENT_CONNECTION) {
+        // Perform SSL handshake
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+            goto cleanup;
+        }
         handle_client_connection(ssl);
     } else if (header.connectionType == SERVER_CONNECTION) {
+        // Set up SSL context to enforce client certificate verification
+        SSL_set_verify(ssl, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+        // Perform SSL handshake
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+            goto cleanup;
+        }
         handle_server_connection(ssl);
     } else {
         // Invalid connection type
     }
 
 cleanup:
+    close(SSL_get_fd(ssl));
     SSL_free(ssl);
     pthread_exit(NULL);
 }
