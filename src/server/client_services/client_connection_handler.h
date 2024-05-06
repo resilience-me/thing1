75fae7749d5ad7057776a9f4c58f88631cca01f4
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 11:05:24 +0200

    Update client_connection_handler.h

--- src/server/client_services/client_connection_handler.h
+++ src/server/client_services/client_connection_handler.h
@@ -4,6 +4,6 @@
 #include <openssl/ssl.h>
 
 // Function to handle client connections
-void *handle_client_connection(void *arg);
+void handle_client_connection(void *arg);
 
 #endif /* CLIENT_CONNECTION_HANDLER_H */
