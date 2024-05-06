ecd8bc442f4737b974a2820add1a963ad9273275
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 07:16:26 +0200

    Update server_as_server_command_handlers.c

--- src/server/server_services/server_as_server/server_as_server_command_handlers.c
+++ src/server/server_services/server_as_server/server_as_server_command_handlers.c
@@ -7,6 +7,7 @@
 #include <unistd.h> // For access and F_OK
 #include <stdio.h> // For sscanf
 #include "database.h"
+#include <stdlib.h>
 
 bool account_exists(char *username) {
     // Check if username is "none", then use default user
@@ -32,24 +33,52 @@ bool account_exists(char *username) {
 }
 
 const char *server_as_server_handle_set_trustline(const char *args) {
-    
-    char username[256];
+    char remote_username[256];
+    char local_username[256];
     char sizeStr[256];
     
     // Parse arguments
-    int parsed_args = sscanf(args, "%255s %255s", username, sizeStr);
+    int parsed_args = sscanf(args, "%255s %255s %255s", remote_username, local_username, sizeStr);
     
     // Check if all arguments were successfully parsed
-    if (parsed_args != 2) {
+    if (parsed_args != 3) {
         return "INVALID_ARGUMENTS";
     }
-    if(!account_exists(username)) return "FAILED_TO_SET_TRUSTLINE";
-    
-    
-      
-    // // Send an error response to the client indicating an invalid command
-    // send_response(ssl, "INVALID_COMMAND");
 
-  
-  return "PLACEHOLDER RETURN STRING";
+    // Check if username is "none", then use default user
+    if (strcmp(local_username, "none") == 0) {
+          username = DEFAULT_USER;
+    }
+
+    // Check if username is valid
+    if (!isValidUsername(local_username)) {
+        return false;
+    }
+    // Build the path to the user directory
+    char user_dir[768];
+    snprintf(user_dir, sizeof(user_dir), "%s/accounts/%s", datadir, local_username);
+
+    // Check if user directory exists
+    if (access(user_dir, F_OK) == -1) {
+        return false;
+    }
+
+    // Construct the file path
+    char filepath[1280];
+    snprintf(filepath, sizeof(filepath), "%s/peers/%s/trustline_incoming", user_dir, remote_username);
+
+    // Open the file in write mode
+    FILE *file = fopen(filepath, "w");
+    if (file == NULL) {
+        return "FAILED_TO_OPEN_FILE";
+    }
+
+    // Write the trustline information to the file
+    fprintf(file, "%s\n", sizeStr);
+
+    // Close the file
+    fclose(file);
+
+    // Return a success message
+    return "TRUSTLINE_SET_SUCCESSFULLY";
 }
