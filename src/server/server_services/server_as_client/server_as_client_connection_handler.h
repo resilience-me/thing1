0d1361e0899a5c4f63369f0692829953b0b5f46d
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 05:44:45 +0200

    Create server_as_client_connection_handler.h

---
+++ src/server/server_services/server_as_client/server_as_client_connection_handler.h
@@ -0,0 +1,6 @@
+#ifndef SERVER_AS_CLIENT_H
+#define SERVER_AS_CLIENT_H
+
+const char* server_as_client_connection_handler(const char *server_address, int port, const char *command, const char *args);
+
+#endif /* SERVER_AS_CLIENT_H */
