71ca7576b4e2f604c68906db8f4808fe1ce89e90
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 05:12:15 +0200

    Update format_utils.h

--- src/common/format_utils.h
+++ src/common/format_utils.h
@@ -1,7 +1,11 @@
 #ifndef FORMAT_UTILS_H
 #define FORMAT_UTILS_H
 
+bool isValidArgument(const char *arg);
+
 // Checks if the given username contains only valid characters
 int isValidUsername(const char *username);
+
+int isValidPort(const char *port);
 
 #endif // FORMAT_UTILS_H
