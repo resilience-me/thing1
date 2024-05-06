58d0c3e4a930e287cc5afc497f721e492204eb87
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 10:56:44 +0200

    Update connection_handler.h

--- src/server/connection_handler.h
+++ src/server/connection_handler.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 // Function prototypes
-void *handle_connection(void *arg);
+void handle_new_connection(int client_sock, SSL_CTX *ctx);
 
 // Add more function prototypes for server-to-client and server-to-server commands...
 
