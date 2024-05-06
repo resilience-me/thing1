67819ba07bbdd96d48400203fe8b6feef9bb46cd
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 06:56:48 +0200

    Update commands_util.c

--- src/server/commands_util.c
+++ src/server/commands_util.c
@@ -37,7 +37,7 @@ Command *client_commands = NULL;
 void initialize_all_commands() {
     client_commands = initialize_commands(client_command_names, client_command_handlers);
     server_as_client_commands = initialize_commands(server_command_names, server_as_client_command_handlers);
-    server_as_server_commands = initialize_commands(server_command_names, server_as_client_command_handlers);
+    server_as_server_commands = initialize_commands(server_command_names, server_as_server_command_handlers);
 }
 
 void free_all_commands() {
