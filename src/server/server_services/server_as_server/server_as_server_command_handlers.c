dffc42a0f8cd510813b9047ad63ff6791851713c
resilience-me <emailsareobsolete@gmail.com>
2024-05-06 06:00:01 +0200

    Update server_as_server_command_handlers.c

--- src/server/server_services/server_as_server/server_as_server_command_handlers.c
+++ src/server/server_services/server_as_server/server_as_server_command_handlers.c
@@ -1,5 +1,5 @@
-#include <openssl/ssl.h>
+#include "server_as_server_command_handlers.h"
 
-const char *server_as_server_handle_set_trustline(SSL *ssl, const char *args) {
+const char *server_as_server_handle_set_trustline(const char *args) {
   return "PLACEHOLDER RETURN STRING";
 }
